3ab4a98da0871c2302eb28fa666983130195fa57
Joshua Yuen <joshua.e.yuen@gmail.com>
2014-03-10 16:20:24 -0700

    mpalocation added to models.py

--- reports/models.py
+++ reports/models.py
@@ -7,6 +7,7 @@ class Report(models.Model):
     resolve_days = models.IntegerField(blank = True, max_length=11)
     num_involved = models.IntegerField(blank = True, max_length=11)
     creature = models.CharField(blank = True, max_length=45)
+    mpalocation = models.CharField(blank = True, max_length=45)
     location = models.CharField(blank = True, max_length=45)
     trial_location = models.CharField(blank = True, max_length=45) #city where crime was tried
     violation_description = models.CharField(blank = True, max_length=500)
